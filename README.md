3869e2fdc1b6e01c5c9fcd4093991e2ef7795c97
Matthew Abrams <mcabrams1@gmail.com>
2016-03-27 13:18:55 -0700

    onetime build tasks and better functional test cmd
    Create a onetime build task in gulp w/o watch. Rename functional test
    command to something easier to type. Functional tests now do a build
    beforehand.
    
    Improve readme to reflect different tools and approaches used.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # TDD Backbone Todo App
 
+A [Backbone](http://backbonejs.org/) Todo app built using [TDD](https://en.wikipedia.org/wiki/Test-driven_development).
+
+Also includes usage of [Docker](https://www.docker.com/), [gulp](http://gulpjs.com/),
+[mocha](https://mochajs.org/), [chai](http://chaijs.com/), [casperjs](http://casperjs.org/),
+and [browserify](http://browserify.org/).
+
 ## Setup
 
 1. Install docker
@@ -13,4 +19,4 @@
 ## Tests
 
 - To run unit tests run `npm test` in docker container.
-- To run functional tests run `npm run functional-test` in docker container.
+- To run functional tests run `npm run functional` in docker container.
--- gulpfile.js
+++ gulpfile.js
@@ -16,30 +16,41 @@ var customOpts = {
   entries: ['./src/js/index.js'],
   debug: true
 };
-var opts = assign({}, watchify.args, customOpts);
-var b = watchify(browserify(opts).transform(stringify()));
+var watchifyOpts = assign({}, watchify.args, customOpts);
+var watchify = watchify(browserify(watchifyOpts).transform(stringify()));
 
-// add transformations here
-// i.e. b.transform(coffeeify);
+gulp.task('js-watch', watchifyBundle); // so you can run `gulp js-watch` to
+                                       // watch+build the file
+watchify.on('update', watchifyBundle); // on any dep update, runs the bundler
+watchify.on('log', gutil.log); // output build logs to terminal
 
-gulp.task('js', bundle); // so you can run `gulp js` to build the file
-b.on('update', bundle); // on any dep update, runs the bundler
-b.on('log', gutil.log); // output build logs to terminal
-
-function bundle() {
-  return b.bundle()
+function watchifyBundle() {
+  return watchify.bundle()
     // log errors if they happen
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('bundle.js'))
     // next line optional, remove if you don't need to buffer file contents
     .pipe(buffer())
-    // optional, remove if you dont want sourcemaps
+       // Add transformation tasks to the pipeline here.
     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-     // Add transformation tasks to the pipeline here.
-      // .pipe(stringify())
     .pipe(sourcemaps.write('./')) // writes .map file
     .pipe(gulp.dest('./dist'));
 }
+
+gulp.task('js', function() {
+  return browserify(customOpts)
+    .transform(stringify())
+    .bundle()
+    // log errors if they happen
+    // .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    // need to buffer file contents so we can use sourcemaps and uglify
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+      // Add transformation tasks to the pipeline here.
+    .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('html', function() {
   gulp.src('./index.html')
